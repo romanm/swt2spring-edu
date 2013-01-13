8a5bedba0f102e1f85aa7c299102a1702ef94762
roman <roman.mishchenko@gmail.com>
2013-01-13 18:09:49 +0100

    CaretSample2 experiment

--- src/main/java/edu/step2/java2s/CaretSample2.java
+++ src/main/java/edu/step2/java2s/CaretSample2.java
@@ -5,6 +5,7 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
@@ -22,91 +23,81 @@ import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class CaretSample2 {
-	private void make(JFrame frame) {
-		JMenuBar mb = new JMenuBar();
-		JMenu mainMenu = new JMenu("Menu");
-		mb.add(mainMenu);
-		frame.setJMenuBar(mb);
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private JTextArea textDebugArea;
+	private JTable regimeTasksTable;
+	JTextField dot, mark ;
+	private JMenu mainMenu;
+	private JToolBar toolBar;
+	
+	public JToolBar getToolBar() {
+		return toolBar;
+	}
+	public JMenu getMainMenu() {
+		return mainMenu;
+	}
+	public CaretSample2(JFrame frame) {
+		mainMenu = new JMenu("File");
+		mainMenu.setMnemonic(KeyEvent.VK_F);
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		Container content = frame.getContentPane();
-
 		JPanel infoPanel = new JPanel();
 		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
-
-		final JTextField dot = new JTextField();
-		final JTextField mark = new JTextField();
-		JToolBar toolBar1 = new JToolBar();
-		addAction(toolBar1,mainMenu, "a1");
-		addAction(toolBar1,mainMenu, "a2");
-		JToolBar toolBar = toolBar1;
-		infoPanel(infoPanel, dot, mark,toolBar);
+		dot = new JTextField();
+		mark = new JTextField();
+		toolBar= new JToolBar();
+		infoPanel(infoPanel, toolBar);
+		JSplitPane splitPane = workPanel();
+		content.add(splitPane, BorderLayout.CENTER);
+		content.add(infoPanel, BorderLayout.SOUTH);
+		
 		CaretListener listener = new CaretListener() {
 			public void caretUpdate(CaretEvent caretEvent) {
 				dot.setText("" + caretEvent.getDot());
 				mark.setText("" + caretEvent.getMark());
 			}
 		};
-
-		JSplitPane splitPane = workPanel2(listener);
-
-		content.add(splitPane, BorderLayout.CENTER);
-		content.add(infoPanel, BorderLayout.SOUTH);
-	}
-	private JSplitPane workPanel2(CaretListener listener) {
-		JPanel sidebarPanel = sidebarPanel2();
-		JTextArea textArea = getTextArea(listener);
-		sidebarPanel.add(textArea);
-		JScrollPane contextPane = contextPanel2();
-		TableSample3 tableSample3 = new TableSample3();
-		JTable makeTable = tableSample3.makeTable();
-		contextPane.setViewportView(makeTable);
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,contextPane,sidebarPanel);
-		splitPane.setDividerLocation(450);
-		return splitPane;
-	}
-	private JSplitPane workPanel(CaretListener listener) {
-		JTextArea textArea = getTextArea(listener);
 		
-		JPanel sidebarPanel = sidebarPanel();
-		JScrollPane contextPane = contextPanel(textArea);
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,contextPane,sidebarPanel);
+		textDebugArea.addCaretListener(listener);
+	}
+	void addMenu(JFrame frame) {
+		JMenuBar mb = new JMenuBar();
+		frame.setJMenuBar(mb);
+		mb.add(mainMenu);
+	}
+	private JSplitPane workPanel() {
+		JPanel sidebarPanel = sidebarPanel2();
+		textDebugArea = new JTextArea();
+//		sidebarPanel.add(new JScrollPane(textDebugArea));
+		sidebarPanel.add(textDebugArea);
+		JScrollPane contextPane = contextPane();
+		regimeTasksTable = new TableSample3().makeTable();
+		regimeTasksTable.getSelectionModel().addListSelectionListener(new RowListener());
+		regimeTasksTable.getColumnModel().getSelectionModel().addListSelectionListener(new ColumnListener());
+		contextPane.setViewportView(regimeTasksTable);
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, contextPane, sidebarPanel);
 		splitPane.setDividerLocation(450);
 		return splitPane;
 	}
+	
 	private JPanel sidebarPanel2() {
 		JPanel sidebarPanel = new JPanel();
 		sidebarPanel.setLayout(new BoxLayout(sidebarPanel, BoxLayout.Y_AXIS));
 		sidebarPanel.add(new Label("l3"));
 		return sidebarPanel;
 	}
-	private JPanel sidebarPanel() {
-		final JTextField taInfo = new JTextField();
-		taInfo.setEditable(false);
-		JPanel sidebarPanel = sidebarPanel2();
-		sidebarPanel.add(taInfo);
-		return sidebarPanel;
-	}
-	private JScrollPane contextPanel(JTextArea textArea) {
+	private JScrollPane contextPane() {
 //		JScrollPane contextPane = new JScrollPane(textArea);
-		JScrollPane contextPane = contextPanel2();
-		contextPane.setViewportView(textArea);
-		return contextPane;
-	}
-	private JScrollPane contextPanel2() {
 		JScrollPane contextPane = new JScrollPane();
 		Dimension minimumSize = new Dimension(100, 50);
 		contextPane.setMinimumSize(minimumSize);
 		return contextPane;
 	}
-	private JTextArea getTextArea(CaretListener listener) {
-		JTextArea textArea = new JTextArea();
-		textArea.addCaretListener(listener);
-		return textArea;
-	}
-	private void infoPanel(JPanel infoPanel, final JTextField dot,final JTextField mark, JToolBar toolBar) {
+	private void infoPanel(JPanel infoPanel, JToolBar toolBar) {
 		infoPanel.add(toolBar);
 		dot.setEditable(false);
 		infoPanel.add(new JLabel("Dot: "));
@@ -115,21 +106,58 @@ public class CaretSample2 {
 		infoPanel.add(new JLabel("Mark: "));
 		infoPanel.add(mark);
 	}
-	private void addAction(JToolBar toolBar, JMenu mainMenu, String a1Str) {
-		AbstractAction action = new AbstractAction(a1Str) {
+	void addAction(JToolBar toolBar, JMenu mainMenu, String actionName) {
+		AbstractAction actionSelf = new AbstractAction(actionName) {
 			public void actionPerformed(ActionEvent actionEvent) {
-				System.out.println(actionEvent.getSource());
+				textDebugArea.append(actionEvent.getSource()+".\n");
 			}
 		};
-		JButton a1Button = toolBar.add(action);
-		a1Button.setText(a1Str);
-		mainMenu.add(action);
+		JButton actionButton = toolBar.add(actionSelf);
+		actionButton.setText(actionName);
+		mainMenu.add(actionSelf);
 	}
+	
+	private class RowListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent event) {
+            if (event.getValueIsAdjusting()) {
+                return;
+            }
+            textDebugArea.append("ROW SELECTION EVENT. \n");
+            outputSelection();
+        }
+    }
+	private class ColumnListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent event) {
+            if (event.getValueIsAdjusting()) {
+                return;
+            }
+            textDebugArea.append("COLUMN SELECTION EVENT. \n");
+            outputSelection();
+        }
+    }
+	private void outputSelection() {
+		textDebugArea.append(String.format("Lead: %d, %d. ",
+				regimeTasksTable.getSelectionModel().getLeadSelectionIndex(),
+				regimeTasksTable.getColumnModel().getSelectionModel().getLeadSelectionIndex()));
+		textDebugArea.append("Rows:");
+        for (int c : regimeTasksTable.getSelectedRows()) {
+        	textDebugArea.append(String.format(" %d", c));
+        }
+        textDebugArea.append(". Columns:");
+        for (int c : regimeTasksTable.getSelectedColumns()) {
+        	textDebugArea.append(String.format(" %d", c));
+        }
+        textDebugArea.append(".\n");
+    }
+	
 	public static void main(String args[]) {
 		JFrame frame = new JFrame("Caret Example");
-		CaretSample2 sc2 = new CaretSample2();
-		sc2.make(frame);
+		CaretSample2 sc2 = new CaretSample2(frame);
+		sc2.addAction(sc2.getToolBar(),sc2.getMainMenu(), "a1");
+		sc2.addAction(sc2.getToolBar(),sc2.getMainMenu(), "a2");
 		frame.setSize(600, 400);
 		frame.setVisible(true);
+		sc2.addMenu(frame);
 	}
+	private CaretSample2() {}
 }
---
+++ src/main/java/edu/step2/java2s/Framework2.java
@@ -0,0 +1,183 @@
+package edu.step2.java2s;
+
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+/* Framework.java requires no other files. */
+public class Framework2 extends WindowAdapter {
+    public int numWindows = 0;
+    private Point lastLocation = null;
+    private int maxX = 500;
+    private int maxY = 500;
+
+    public Framework2() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        maxX = screenSize.width - 50;
+        maxY = screenSize.height - 50;
+        makeNewWindow();
+    }
+    public void makeNewWindow() {
+    	numWindows++;
+		JFrame frame = new JFrame("Caret Example "+numWindows);
+        frame.addWindowListener(this);
+		CaretSample2 sc2 = new CaretSample2(frame);
+		sc2.addAction(sc2.getToolBar(),sc2.getMainMenu(), "a1");
+		sc2.addAction(sc2.getToolBar(),sc2.getMainMenu(), "a2");
+		frame.setSize(600, 400);
+		addWindowMenuItems(sc2.getMainMenu(),this,frame);
+		sc2.addMenu(frame);
+		setLocation(frame);
+		frame.setVisible(true);
+    }
+    public void makeNewWindow3() {
+        JFrame frame = new MyFrame(this);
+        numWindows++;
+        System.out.println("Number of windows: " + numWindows);
+        setLocation(frame);
+        frame.setVisible(true);
+    }
+
+	private void setLocation(JFrame frame) {
+		if (lastLocation != null) {
+            //Move the window over and down 40 pixels.
+            lastLocation.translate(40, 40);
+            if ((lastLocation.x > maxX) || (lastLocation.y > maxY)) {
+                lastLocation.setLocation(0, 0);
+            }
+            frame.setLocation(lastLocation);
+        } else {
+            lastLocation = frame.getLocation();
+        }
+
+        System.out.println("Frame location: " + lastLocation);
+	}
+
+    //This method must be evoked from the event-dispatching thread.
+    public void quit(JFrame frame) {
+        if (quitConfirmed(frame)) {
+            System.out.println("Quitting.");
+            System.exit(0);
+        }
+        System.out.println("Quit operation not confirmed; staying alive.");
+    }
+
+    private boolean quitConfirmed(JFrame frame) {
+        String s1 = "Quit";
+        String s2 = "Cancel";
+        Object[] options = {s1, s2};
+        int n = JOptionPane.showOptionDialog(frame,
+                "Windows are still open.\nDo you really want to quit?",
+                "Quit Confirmation",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                s1);
+        if (n == JOptionPane.YES_OPTION) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void windowClosed(WindowEvent e) {
+        numWindows--;
+        System.out.println("Number of windows = " + numWindows);
+        if (numWindows <= 0) {
+            System.out.println("All windows gone.  Bye bye!");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        Framework2 framework = new Framework2();
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    class MyFrame extends JFrame {
+        protected Dimension defaultSize = new Dimension(200, 200);
+        protected Framework2 framework = null;
+		private JMenu menu;
+        public JMenu getMenu() {return menu;}
+
+		public MyFrame(Framework2 controller) {
+            super("New Frame");
+            framework = controller;
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            addWindowListener(framework);
+            
+            JMenuBar menuBar = new JMenuBar();
+            setJMenuBar(menuBar);
+            menu = new JMenu("Window");
+            menu.setMnemonic(KeyEvent.VK_W);
+            addWindowMenuItems(menu,framework,this);
+            menuBar.add(menu);
+            setSize(defaultSize);
+        }
+
+    }
+		private void addWindowMenuItems(JMenu menu, final Framework2 framework, final JFrame myFrame) {
+			JMenuItem item = null;
+            //close
+            item = new JMenuItem("Close");
+            item.setMnemonic(KeyEvent.VK_C);
+            item.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Close window");
+                    myFrame.setVisible(false);
+                    myFrame.dispose();
+                }
+            });
+            menu.add(item);
+
+            //new
+            item = new JMenuItem("New");
+            item.setMnemonic(KeyEvent.VK_N);
+            item.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("New window");
+                    framework.makeNewWindow();
+                }
+            });
+            menu.add(item);
+
+            //quit
+            item = new JMenuItem("Quit");
+            item.setMnemonic(KeyEvent.VK_Q);
+            item.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Quit request");
+                    framework.quit(myFrame);
+                }
+            });
+            menu.add(item);
+    }
+}
+
